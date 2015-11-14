af03415c59dc46ad685f322759db8b695a58fa02
Nathan Noble <nathan.noble@gmail.com>
2015-11-13 22:37:34 -0500

    nothing much

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+  
+  #http_basic_authenticate_with name: "gwii", password: "iiwg", except: :index
+  before_filter :restrict_access, except: :index
+
+
+  private
+    def restrict_access
+      authenticate_or_request_with_http_token do |token, options|      
+        answer = ENV["SECRET_KEY_BASE"]
+      end
+    end
 end
---
+++ app/views/authors/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @author, :id, :email, :alias, :created_at, :updated_at
